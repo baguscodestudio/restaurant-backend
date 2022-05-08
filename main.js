78136fd180e30cc59dffb5acc8ceb18d8373a648
BagusCodeStudio <60974759+baguscodestudio@users.noreply.github.com>
2022-05-08 20:10:09 +0800

    Add item management.

--- main.js
+++ main.js
@@ -339,6 +339,96 @@ app.post("/removeRole", async (req, res, next) => {
   }
 });
 
+app.get("/items", async (req, res) => {
+  const [result, field] = await connection.execute("SELECT * FROM item");
+  res.json(result);
+});
+
+app.post("/addItem", async (req, res) => {
+  const { name, price, category, description, photo } = req.body;
+
+  if (name && price && category && description && photo) {
+    const [result, field] = await connection.execute(
+      "INSERT INTO item (name, price, description, category, photo) VALUES (?,?,?,?,?)",
+      [name, price, description, category, photo]
+    );
+
+    if (result.affectedRows > 0) {
+      res.json({
+        success: true,
+        message: "Added item successfully",
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        message: "No rows were updated",
+      });
+    }
+  } else {
+    res.status(400).json({
+      success: false,
+      message:
+        "Incomplete arguments, expected 5 (name, price, category, description, photo)",
+    });
+  }
+});
+
+app.post("/updateItem", async (req, res) => {
+  const { itemid, name, price, category, description, photo } = req.body;
+  if (itemid && name && price && category && description && photo) {
+    const [result, field] = await connection.execute(
+      "UPDATE item SET name=?, price=?,description=?,category=?,photo=? WHERE itemid=?",
+      [name, price, description, category, photo, itemid]
+    );
+
+    if (result.affectedRows > 0) {
+      res.json({
+        success: true,
+        message: "Added item successfully",
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        message: "No rows were updated",
+      });
+    }
+  } else {
+    res.status(400).json({
+      success: false,
+      message:
+        "Incomplete arguments, expected 6 (itemid, name, price, category, description, photo)",
+    });
+  }
+});
+
+app.post("/removeItem", async (req, res) => {
+  const { itemid } = req.body;
+
+  if (itemid) {
+    const [result, field] = await connection.execute(
+      "DELETE FROM item WHERE itemid=?",
+      [itemid]
+    );
+
+    if (result.affectedRows > 0) {
+      res.json({
+        success: true,
+        message: "Removed item successfully",
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        message: "No rows were updated",
+      });
+    }
+  } else {
+    res.status(400).json({
+      success: false,
+      message: "Incomplete arguments, expected 1 (itemid)",
+    });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
